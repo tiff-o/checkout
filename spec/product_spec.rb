4425b01c17b7bfa28a3e314591053d1b1f3024d6
Tiffany Duong <tiff4ny.dev@gmail.com>
2020-10-13 18:50:31 +1100

    added show to product spec - tests passing

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,16 +1,29 @@
 require_relative 'spec_helper'
-require_relative '../lib/product'
+require_relative '../lib/models/product'
 
 describe Product do
+  before do
+    @uuid = 1411
+    @name = "Jockey Wheels - Orange"
+    @price = 15.39
+  end
+
   describe "#initialize" do
-    let(:uuid) { 1411 }
-    let(:name) { "Jockey Wheels - Orange" }
-    let(:price) { "15.39" }
-    let(:product) { Product.new(uuid: uuid, name: name, price: price) }
+
+    let(:product) { Product.new(uuid: @uuid, name: @name, price: @price) }
 
     it "should create a product" do
-
       expect(product.name).to eq "Jockey Wheels - Orange"
     end
   end
+
+  describe ".show" do
+
+    it "should show individual product details" do
+      @product = Product.new(uuid: @uuid, name: @name, price: @price)
+
+      expect(@product.show.name).to eq "Jockey Wheels - Orange"
+    end
+  end
+
 end

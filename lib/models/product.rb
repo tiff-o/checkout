96bbbbb10e9d31daa495e3b6ba6855fc2e9adaa5
Tiffany Duong <tiff4ny.dev@gmail.com>
2020-10-13 18:52:18 +1100

    removed uneccessary code from product model

--- lib/models/product.rb
+++ lib/models/product.rb
@@ -1,6 +1,3 @@
-require_relative 'catalogue'
-# show product details
-
 class Product
   attr_reader :uuid, :name, :price
 
@@ -13,12 +10,4 @@ class Product
   def show
     @product = Product.new(uuid: @uuid, name: @name, price: @price)
   end
-
-  def remove_from_cart
-    # list products in cart
-    # user chooses product by index + 1
-    # delete instance of product from cart
-    # update_cart
-  end
-
 end

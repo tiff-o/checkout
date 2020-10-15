496d3aae34e3d1c4c355b6e532b7e792817d5cca
Tiffany Duong <tiff4ny.dev@gmail.com>
2020-10-15 11:50:39 +1100

    and another fix from previous commit

--- lib/app.rb
+++ lib/app.rb
@@ -8,13 +8,13 @@ require_relative 'views/shopping_cart_view'
 require_relative 'router'
 
 catalogue_file = File.read('lib/products.json')
-@catalogue = Catalogue.new(catalogue_file)
-catalogue_controller = CatalogueController.new(catalogue: @catalogue)
+catalogue = Catalogue.new(catalogue_file)
+catalogue_controller = CatalogueController.new(catalogue: catalogue)
 
 shopping_cart_file = File.read('lib/shopping_cart_products.json')
-@shopping_cart = ShoppingCart.new(shopping_cart_file)
-shopping_cart_controller = ShoppingCartController.new(shopping_cart: @shopping_cart)
+shopping_cart = ShoppingCart.new(shopping_cart_file)
+shopping_cart_controller = ShoppingCartController.new(shopping_cart: shopping_cart)
 
-router = Router.new(catalogue_controller: @catalogue_controller, shopping_cart_controller: @shopping_cart_controller)
+router = Router.new(catalogue_controller: catalogue_controller, shopping_cart_controller: shopping_cart_controller)
 
 router.run
